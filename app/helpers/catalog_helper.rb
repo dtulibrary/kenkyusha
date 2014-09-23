1d4e7d716450654f5af7550247768e5da4ba107c
Jimmy Petersen <jipe@dtic.dtu.dk>
2014-09-23 09:55:58 +0200

    Images, affiliations, fields

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -6,24 +6,30 @@ module CatalogHelper
   end
 
   def render_source_field args
+    args[:document]['source_ss'].collect {|s| t "source_labels.#{s}"}.join ' ; '
+  end 
 
-    case t "source_labels.#{args[:document]['source_ss']}"
-    when /["Orbit"]/
-      'Technical University of Denmark'
-    when /["Rdb Ku"]/
-       'University of Copenhagen'
-    when /["Rdb Itu"]/
-       'IT University of Copenhagen'
-    when /["Rdb Au"]/
-       'Aarhus University'
-    when /["Rdb Cbs"]/
-       'Copenhagen Business School'
-    when /["Rdb Ruc"]/
-       'Roskilde University'
-    when /["Rdb Sdu"]/
-       'University of Southern Denmark'
-    when /["Rdb Vbn"]/
-       'Aalborg University'
-    end 
+  def create_researcher_image_url document
+    t "image_urls.#{document['source_ss'].first}", :id => document['cris_id_ssf'].first if document['cris_id_ssf']
+  end
+
+  def render_person_affiliations_index document
+    affiliations = []
+    json = JSON.parse document['person_affiliations_ssf'].first
+    json.each do |affiliation|
+      if affiliation['type'] == 'current'
+        affiliation['organisation']['names'].each do |name|
+          if name['lang'] == 'eng'
+            names = []
+            (1..4).each {|i| names << name["level#{i}"]}
+            affiliations << names.compact.join(' &mdash; ')
+          end
+        end
+      end
+    end
+    affiliations.join('<br>').html_safe
+  end
+
+  def render_person_affiliations_show document
   end
 end
