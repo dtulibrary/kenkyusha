818dce88670cee8d01cc4d6bc159f513ffa4c09a
Jimmy Petersen <jipe@dtic.dtu.dk>
2014-10-06 10:33:33 +0200

    Force IE out of compatibility view

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Kenkyusha
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_dispatch.default_headers.merge! 'X-UA-Compatible' => 'IE=edge'
   end
 end
