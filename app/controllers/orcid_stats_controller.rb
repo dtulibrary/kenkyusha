b2b134d3a8778015d364b07d94e4ceda6c6ee41d
Jimmy Petersen <jipe@dtic.dtu.dk>
2015-08-24 09:25:20 +0200

    Only use Monday in ORCID stats

--- app/controllers/orcid_stats_controller.rb
+++ app/controllers/orcid_stats_controller.rb
@@ -2,7 +2,7 @@ class OrcidStatsController < ApplicationController
 
   def index
     response.headers['Access-Control-Allow-Origin'] = '*'
-    render :json => OrcidStat.order('created_at asc')
+    render :json => OrcidStat.where('extract(dow from created_at) = 1').order('created_at asc')
   end
 
 end
