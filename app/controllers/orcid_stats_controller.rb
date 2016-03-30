26da3e87d968965d88df0c0ab167c807cfed7d1f
Jimmy Petersen <jipe@dtu.dk>
2016-03-30 09:01:34 +0200

    Make code more readable

--- app/controllers/orcid_stats_controller.rb
+++ app/controllers/orcid_stats_controller.rb
@@ -5,12 +5,16 @@ class OrcidStatsController < ApplicationController
     stat_dates = []
     stat_dates << {:dow => 1}
     stat_dates << {:year => 2016, :month => 3, :day => 31}
-    stat_dates_clause = stat_dates.map {|date| "(#{stat_date_clause(date)})"}.join(" OR ")
-    render :json => OrcidStat.where(stat_dates_clause).order('created_at asc')
+    stat_dates_clause = stat_dates.map {|date| "(#{stat_date_clause(date)})"}
+                                  .join(" OR ")
+    render :json => OrcidStat.where(stat_dates_clause)
+                             .order('created_at asc')
   end
 
   private
     def stat_date_clause(date)
-      [:year, :month, :day, :dow].reject {|part| date[part].nil? }.map {|part| "extract(#{part} from created_at) = #{date[part]}"}.join(' AND ')
+      [:year, :month, :day, :dow].reject {|part| date[part].nil?}
+                                 .map {|part| "extract(#{part} from created_at) = #{date[part]}"}
+                                 .join(' AND ')
     end
 end
